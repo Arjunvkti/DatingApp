1147ef5bc33049b0e9c0fc74319ae5b197eb5d6c
Arjun Vijay <arjunvjy95@gmail.com>
2021-07-25 00:53:27 +0530

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -37,22 +43,17 @@ namespace API
             services.AddCors();
             
             services.AddMvc();
-
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-
-            });
             
+            services.AddApplicationService(_config);
 
             services.AddControllers();
-
-            
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+           services.AddIdentityServices(_config);
 
         }
 
@@ -73,6 +74,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors(pol =>  pol.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
