3d12a7be60e8abbba9a8af626ca3ace71bee7e1a
Arjun Vijay <arjunvjy95@gmail.com>
2021-08-12 20:32:59 +0530

    section 19 - pre switch to postgres

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,11 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Middleware;
+using API.Services;
+using API.SignalR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using API.Extensions;
-using API.Middleware;
-using API.SignalR;
 
 namespace API
 {
@@ -16,32 +31,20 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-
-
         }
-
-
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
-            services.AddCors();
-            
-            services.AddMvc();
-            
             services.AddApplicationService(_config);
-
             services.AddControllers();
-
+            services.AddCors();
+            services.AddIdentityServices(_config);
+            services.AddSignalR();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
-
-           services.AddIdentityServices(_config);
-           services.AddSignalR();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
