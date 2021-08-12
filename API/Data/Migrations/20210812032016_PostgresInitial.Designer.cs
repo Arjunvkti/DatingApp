e757aa1ffeed63d5507e4f1e71e5fc5b9c5c671e
Arjun Vijay <arjunvjy95@gmail.com>
2021-08-12 09:18:13 +0530

    Add precompiled assets for Heroku

--- API/Data/Migrations/20210812032016_PostgresInitial.Designer.cs
+++ API/Data/Migrations/20210812032016_PostgresInitial.Designer.cs
@@ -10,7 +10,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20210811023437_PostgresInitial")]
+    [Migration("20210812032016_PostgresInitial")]
     partial class PostgresInitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
